29284b25156d69f1e4779bfbf34661f53ff6b563
cd-x <whowantsthisemailid@gmail.com>
2024-01-16 17:28:42 +0530

    server is up

--- greet/server/main.go
+++ greet/server/main.go
@@ -2,8 +2,12 @@ package main
 import (
 	"log"
 	"net"
+	"google.golang.org/grpc"
 	pb "github.com/cd-x/grpc-go/greet/proto"
 )
+type Server struct{
+	pb.GreetServiceServer
+}
 var addr string = "0.0.0.0:50051"
 
 func main() {
@@ -14,4 +18,9 @@ func main() {
 	}
 
 	log.Printf("Listening on: %v\n",addr)
+	s:= grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("Failed to serve: %v\n", err);
+	}
 }
